1f7d3be15285a9944c792f3b53e039c79788cd42
Ajizu1985 <az3406.aa@gmail.com>
2021-12-12 13:33:43 +0500

    Commit

--- 1.c
+++ 1.c
@@ -8,12 +8,14 @@ int main ()
 
     for(i=0; i<5; i++)
     {
-    printf("Enter %d element:", i);
+    printf("Enter %d element:", i+1);
     scanf("%d", &arr[i]);
     }
     for(i=0; i<5; i++)
     {
+    if(arr[i]%2==1){
     printf("arr[%d]=%d\t", i, arr[i]);
+    }
     }
     return 0;
 }
